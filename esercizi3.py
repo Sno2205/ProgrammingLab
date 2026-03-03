9d2aab40fbc4cc66037eaa86d0e3825cfb3b6130
Xinhao <cxinhao2205@gmail.com>
2026-03-03 02:30:19 +0100

    Svolti tutti gli es fino alla Lezione 3

--- esercizi3.py
+++ esercizi3.py
@@ -40,6 +40,58 @@ def sommaVenditeShampo():
     print(f"La somma delle vendite è {somma}")
     return somma
 
+#Funzione creazione file con contenuto scritto in input da utente
+def scritturaFile():
+    #frase = input("Dammi una frase da scrivere su file!!\n")
+    frase = "\nIl gatto e la volpe sono animali.\nIl gatto vive in casa, mentre la volpe vive nel bosco.\nLa volpe e' furba, ma il gatto e' agile.\nOggi ho visto un gatto nel giardino."
+    my_file = open('test.txt', 'a')
+    my_file.write(frase)
+    my_file.close()
 
 
-sommaVenditeShampo()
+#Funzione che preso un file conta il numero di volte in cui una parola è scritta (deve essere esattamente quella)
+def ricorrenzaParolaFile():
+    parola = input("Dammi un parola!!")
+    cont = 0
+    my_file = open('test.txt', 'r')
+    for line in my_file:
+        elements = line.split(' ')
+        for i in elements[:-1]:
+            if(parola == i):
+                cont += 1
+
+    print(f"Numero di ricorrenze di {parola} = {cont}")
+    my_file.close()
+    
+#Funzione che preso un file crea un dizionario con quante volta c'è una parola (chiave)
+def fileToDict():
+    wordDict = {}
+    my_file = open('test.txt', 'r')
+    for line in my_file:
+        elements = line.split(' ')
+        for i in elements[:-1]:
+            if(i in wordDict):
+                wordDict[i] +=1
+            else:
+                wordDict[i] = 1
+        
+    for chiave, valore in wordDict.items():
+        print(f"{chiave}: {valore}")
+    my_file.close()
+
+#Funzione che preso un file e rimuove tutte le righe duplicate
+def createUnique():
+    values = []
+    my_file = open('test.txt', 'r')
+    my_file2 = open('unique.txt', 'x')
+    
+    for line in my_file:
+        if not (line in values):
+            my_file2.write(line)
+            values.append(line)
+
+    my_file.close()
+    my_file2.close()
+
+
+createUnique()
